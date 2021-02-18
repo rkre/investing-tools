420aa2d2ce5a664362577d0b0deab1d4c54b37df
Rachel <r.kreynin@gmail.com>
2021-02-18 13:38:39 -0500

    update

--- secrets.py
+++ secrets.py
@@ -1,11 +1,13 @@
+#IEX Sandbox tokens
 #IEX_CLOUD_API_TOKEN = 'Tpk_059b97af715d417d9f49f50b51b1c448'
 #IEX_CLOUD_API_TOKEN = 'Tpk_7112c5d539334780aa5cd0e569f8e710'
 
 # API Keys and user login info go here
+# From iexcloud.io
 IEX_CLOUD_API_TOKEN = 
+# From Robinhood.com 
 RH_USER_EMAIL = 
 RH_PASSWORD = 
 
 # This code is generated if you do MFA/2FA with an authenticator app for Robinhood
 RH_MFA_CODE = 
-
