ae9842f731958e339dcff951d950ce7c9ade6a13
Rachel <rkreynin@gmail.com>
2021-04-27 16:06:20 -0400

    updates

--- iron_corridor.py
+++ iron_corridor.py
@@ -5,7 +5,7 @@ from secrets import IEX_CLOUD_API_TOKEN
 import requests
 import pandas as pd
 from stock_scanner import latest_price
-import volatility_scanner
+import volatility_scanner as vs
 
 # Getting the following from the IEX API
 # https://cloud.iexapis.com/ 
@@ -19,15 +19,20 @@ def iron_corridor(ticker):
     current_price = float(current_price)
     # Average volatility 
     avg_vol = .03 # 10%
-    max_vol = .04 
+    max_vol = vs.highest_volatility(ticker)
 
     # Calculate strike prices to buy_put, sell_put, sell_call, buy_call
-    sell_put_strike = -(current_price * avg_vol) + current_price
+    sell_put_strike = -(current_price * max_vol) + current_price
     buy_put_strike = -(sell_put_strike * (max_vol-avg_vol)) + sell_put_strike
-    sell_call_strike = (current_price * avg_vol) + current_price
+    sell_call_strike = (current_price * max_vol) + current_price
     buy_call_strike = (sell_call_strike * (max_vol-avg_vol)) + sell_call_strike
+    # sell_put_strike = -(current_price * avg_vol) + current_price
+    # buy_put_strike = -(sell_put_strike * (max_vol-avg_vol)) + sell_put_strike
+    # sell_call_strike = (current_price * avg_vol) + current_price
+    # buy_call_strike = (sell_call_strike * (max_vol-avg_vol)) + sell_call_strike
 
     # Find premium fees for selected strike prices expiring the same week
+    print(sell_put_strike,sell_call_strike)
 
     IC_strikes = [sell_put_strike, buy_put_strike, sell_call_strike, buy_call_strike]
     return IC_strikes
--- volatility_scanner.py
+++ volatility_scanner.py
@@ -25,7 +25,7 @@ market_api_url = 'https://cloud.iexapis.com/stable/stock/market/list'
 stock_api_url = 'https://cloud.iexapis.com/stable/stock'
 
 def get_1yr_data(ticker):
-    url_1yr = f'{stock_api_url}/{ticker}/chart/6m?token={IEX_CLOUD_API_TOKEN}'
+    url_1yr = f'{stock_api_url}/{ticker}/chart/5y?token={IEX_CLOUD_API_TOKEN}'
     year_data = requests.get(url_1yr).json()
     year_df = pd.DataFrame(year_data)
     pd.set_option("display.max_rows", None, "display.max_columns", None)
@@ -44,34 +44,34 @@ def daily_volatility(ticker):
     highlows = get_daily_highlows(ticker)
     points_change = highlows['high'].sub(highlows['low'])
     highlows.insert(3,"points",points_change)
-    percent_change = points_change.div(highlows['low'])*100
+    percent_change = points_change.div(highlows['low'])
     highlows.insert(4,"%",percent_change)
     return highlows
 
 def highest_volatility(ticker):
     "Returns the date annd prices for the highest volatility of a stock"
     highlows = daily_volatility(ticker)
-    highest_vol = highlows.nlargest(5,"%")
-    return highest_vol
+    highest_vol = highlows.nlargest(1,"%")
+    return highest_vol.iloc[0,4]
 
 def highest_price(ticker):
     "Returns highest price in a given time period"
     prices = get_daily_highlows(ticker)
-    highest_price = prices.nlargest(2,"high")
-    return highest_price
+    highest_price = prices.nlargest(1,"high")
+    return highest_price.iloc[0,1]
 
 def lowest_price(ticker):
     "Returns lowest price in a given time period"
     prices = get_daily_highlows(ticker)
-    lowest_price = prices.nsmallest(2,"high")
-    return lowest_price
+    lowest_price = prices.nsmallest(1,"high")
+    return lowest_price.iloc[0,2]
 
-# ticker = 'MSFT'
-# highlow = highest_volatility(ticker)
+ticker = 'MSFT'
+highlow = highest_volatility(ticker)
 
-# print(highest_price(ticker))
-# print(lowest_price(ticker))
-# print(highlow)
+print(highest_price(ticker))
+print(lowest_price(ticker))
+print(highlow)
 
 
 
