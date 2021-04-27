fdd844189c8a51f2caf8b15a1c32ac5055523322
Rachel <r.kreynin@gmail.com>
2021-04-27 00:31:56 -0400

    volatility analysis script in progress

--- volatility_scanner.py
+++ volatility_scanner.py
@@ -8,11 +8,52 @@
 
 # This data will be used to calculate the right stocks for volatility option trading strategies like Iron Condor
 
-import stock_scanner as ss
-import price_target_scanner as pts
-import options_scanner as ops
+
+from secrets import IEX_CLOUD_API_TOKEN
+import numpy as np
+import pandas as pd
+import requests #for http requests
 
 
 # Function to find closing price 
 # IEX or RH or yfinance or other API can be used
+
+# Getting the following from the IEX API
+# https://cloud.iexapis.com/ 
+market_api_url = 'https://cloud.iexapis.com/stable/stock/market/list'
+stock_api_url = 'https://cloud.iexapis.com/stable/stock'
+
+def get_1yr_data(ticker):
+    url_1yr = f'{stock_api_url}/{ticker}/chart/6m?token={IEX_CLOUD_API_TOKEN}'
+    year_data = requests.get(url_1yr).json()
+    year_df = pd.DataFrame(year_data)
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+    return year_df
+
+
+def get_daily_highlows(ticker):
+    "Gets the max and min price for the day"
+    year_data = get_1yr_data(ticker)
+    highlows = year_data[['date','high','low']]
+
+    return highlows
+
+def daily_volatility(ticker):
+    "Returns the % of volatility based on the high and low price of the day"
+    highlows = get_daily_highlows(ticker)
+    points_change = highlows['high'].sub(highlows['low'])
+    highlows.insert(3,"points",points_change)
+    percent_change = points_change.div(highlows['low'])*100
+
+    highlows.insert(4,"%",percent_change)
+
+    return highlows
+
+
+highlow = daily_volatility('MSFT')
+print(highlow)
+
+
+
 
